e42fdff33ac8e8a31efb50397fdd8d9963bf3581
lmm-git <leomarschke@arcor.de>
2012-10-11 22:11:56 +0200

    add CalcAcceleration

--- src/modules/WebMath/lib/WebMath/Form/Handler/User/CalcAcceleration.php
+++ src/modules/WebMath/lib/WebMath/Form/Handler/User/CalcAcceleration.php
@@ -41,9 +41,89 @@ class WebMath_Form_Handler_User_CalcAcceleration extends Zikula_Form_AbstractHan
 			return false;
 		}
 		$data = $view->getValues();
-
-
 		
+		//if all required data given
+		if($data['cw'] != '' && $data['density'] != '' && $data['area'] != '' && $data['mass'] != '')
+		{
+			//Push first item
+			$value['0'] = '0';
+			//i = 0.05 because first item was already pushed
+			for($i = 0.05; $i < $data['time']; $i += 0.05)
+			{
+				//calculate rest of items
+				$fr = ($data['mass'] * $data['acceleration']) - (0.5 * $data['cw'] * $data['density'] * $data['area'] * pow($v, 2));
+				$v += $fr / $data['mass'] * 0.05;
+				$s += $v * 0.05;
+				$result = $s;
+				//push result to value
+				$value[(string)$i] = $result;
+			}
+		}
+		else
+			//easier computation
+			for($i = 0; $i < $data['time']; $i += 0.05)
+			{
+				$result = $data['acceleration'] / 2 * pow($i, 2);
+			
+				$value[(string)$i] = $result;
+			}
+		
+		$view->assign('distanceValue', $value);
+
+		$view->assign('distanceGraph', ModUtil::apiFunc('ZjqPlot', 'graph', 'lineGraph', array('data' => array(0 => $value),
+			'title' => $this->__('Distance'),
+			'showMarker' => false,
+			'showHighlighter' => true,
+			'lineWidth' => 1.8,
+			'xaxisTitle' => $this->__('Time in seconds'),
+			'xaxisMin' => '0',
+			'yaxisTitle' => $this->__('Distance in meters'),
+			'yaxisMin' => '0')));
+
+		//Reset vars
+		unset($v);
+		unset($fr);
+		unset($s);
+		unset($value);
+		unset($result);
+
+		//if all required data given
+		if($data['cw'] != '' && $data['density'] != '' && $data['area'] != '' && $data['mass'] != '')
+		{
+			//Push first item
+			$value['0'] = '0';
+			//i = 0.05 because first item was already pushed
+			for($i = 0.05; $i < $data['time']; $i += 0.05)
+			{
+				$fr = ($data['mass'] * $data['acceleration']) - (0.5 * $data['cw'] * $data['density'] * $data['area'] * pow($v, 2));
+				$v += $fr / $data['mass'] * 0.05;
+				$result = $v;
+				$value[(string)$i] = $result;
+			}
+		}
+		else
+			//linear speed
+			for($i = 0; $i < $data['time']; $i += 0.05)
+			{
+				$result = $data['acceleration'] * $i;
+			
+				$value[(string)$i] = $result;
+			}
+		
+		//assign for template
+		$view->assign('speedValue', $value);
+
+		//draw graph
+		$view->assign('speedGraph', ModUtil::apiFunc('ZjqPlot', 'graph', 'lineGraph', array('data' => array(0 => $value),
+			'title' => $this->__('Speed'),
+			'showMarker' => false,
+			'showHighlighter' => true,
+			'lineWidth' => 1.8,
+			'xaxisTitle' => $this->__('Time in seconds'),
+			'xaxisMin' => '0',
+			'yaxisTitle' => $this->__('Speed in m/s'),
+			'yaxisMin' => '0')));
+
 		return true;
 	}
 }
--- src/modules/WebMath/templates/User/CalcAcceleration.tpl
+++ src/modules/WebMath/templates/User/CalcAcceleration.tpl
@@ -21,8 +21,80 @@
 		{formfloatinput id="time" mandatory=true text=10}
 	</div>
 
+</fieldset>
+
+<fieldset>
+	<legend>{gt text='Optional information'}</legend>
+
+	<div class="z-formrow">
+		{formlabel for="cw" __text='Coefficient of drag c_w'}
+		{formfloatinput id="cw"}
+	</div>
+
+	<div class="z-formrow">
+		{formlabel for="density" __text='Density d'}
+		{formfloatinput id="density"}
+	</div>
+
+	<div class="z-formrow">
+		{formlabel for="area" __text='Cross-section area A'}
+		{formfloatinput id="area"}
+	</div>
+
+	<div class="z-formrow">
+		{formlabel for="mass" __text='Mass of the object m'}
+		{formfloatinput id="mass"}
+	</div>
+
 
 </fieldset>
+
+
+{$distanceGraph}
+{if $distanceGraph}
+	<a href="javascript:void(0);" onclick="document.getElementById('distanceTable').style.display = ''; this.style.display = 'none';">{gt text="Show datatable"}</a>
+
+	<table id="distanceTable" class="z-datatable" style="width: 100%; display: none;" width="100%; border: 1px solid grey;">
+		<thead style="width: 100%;">
+			<tr>
+				<th>{gt text="x"}</th>
+				<th>{gt text="y"}</th>
+			</tr>
+		</thead>
+		<tbody id="WorkerPlan_Conditions_TableBody">
+		
+			{foreach from=$distanceValue item='item' key='key'}
+				<tr class="{cycle values='z-odd,z-even'}">
+					<td>{$key}</td>
+					<td>{$item}</td>
+				</tr>
+			{/foreach}
+		</tbody>
+	</table>
+{/if}
+
+{$speedGraph}
+
+{if $speedGraph}
+	<a href="javascript:void(0);" onclick="document.getElementById('speedTable').style.display = ''; this.style.display = 'none';">{gt text="Show datatable"}</a>
+	<table id="speedTable" class="z-datatable" style="width: 100%; display: none;" width="100%; border: 1px solid grey;">
+		<thead style="width: 100%;">
+			<tr>
+				<th>{gt text="x"}</th>
+				<th>{gt text="y"}</th>
+			</tr>
+		</thead>
+		<tbody id="WorkerPlan_Conditions_TableBody">
+		
+			{foreach from=$speedValue item='item' key='key'}
+				<tr class="{cycle values='z-odd,z-even'}">
+					<td>{$key}</td>
+					<td>{$item}</td>
+				</tr>
+			{/foreach}
+		</tbody>
+	</table>
+{/if}
 
 
 <div class="z-formbuttons z-buttons">
@@ -31,5 +103,7 @@
 </div>
 
 {/form}
+
+
 
 {include file='User/Includes/Footer.tpl'}
